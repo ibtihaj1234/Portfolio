d87f051889e6a491e615bad2de68bdc76bc139be
ibtihaj1234 <127891292+ibtihaj1234@users.noreply.github.com>
2023-08-23 22:50:15 +0500

    animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 const Intro = () => {
     return (
         <div className='Intro'>
+            <div style={{lineHeight:1.5}}>
+                <h1 className='typed-text' style={{ color: 'orangered' }}>
+                    WELCOME TO MY PROFILE
+                </h1>
+            </div>
             <div className='Picture'>
                 <img src={require('../../assets/MyPic.jpg')} alt='12' />
             </div>
             <div className='Introduction'>
-                <h1 style={{ color: 'orangered' }}>
-                    WELCOME TO MY PROFILE
-                </h1>
                 <p style={{ fontSize: 'X-Large', color: 'black' }}>This is Ibtihaj Saleem a BS-Comuter Science studnet at Hamdard University.</p>
                 <p style={{ fontSize: 'X-Large', color: 'black' }}>Currently a Development Intern at 3Const Pvt (ltd). </p>
             </div>
