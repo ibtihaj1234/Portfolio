702002dc269ca1bb868abb67fdba5624ee52e322
ibtihaj1234 <127891292+ibtihaj1234@users.noreply.github.com>
2023-08-29 18:58:58 +0500

    Home page upgraded

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Intro = () => {
     return (
         <div className='Intro'>
-            <div style={{lineHeight:1.5}}>
+            <div style={{ lineHeight: 1.5 }}>
                 <h1 className='typed-text' style={{ color: 'orangered' }}>
                     WELCOME TO MY PROFILE
                 </h1>
@@ -12,8 +12,27 @@ const Intro = () => {
                 <img src={require('../../assets/MyPic.jpg')} alt='12' />
             </div>
             <div className='Introduction'>
-                <p style={{ fontSize: 'X-Large', color: 'black' }}>This is Ibtihaj Saleem a BS-Comuter Science studnet at Hamdard University.</p>
-                <p style={{ fontSize: 'X-Large', color: 'black' }}>A Front-end Developer at 3Const Pvt (ltd). </p>
+                <p style={{ fontSize: 'X-Large', color: 'black' }}>Hey There!! I am Ibtihaj Saleem a full-stack web-developer. I have learned HTML5, CSS3, JS, PHP, and MySQL from Aptech Learnig Center Pakistan. Right now i am working on React.js as a frontend framework for a company in Pakistan. Griefly want to learn backend frameworks like django and Laravel. Along with it i am working on Mobile app development with React Native framework </p>
+            </div>
+            <div className='briefing'>
+                <div className='heading'>
+                    <h1 className='typed-text' style={{ color: 'orangered' }}>More About ME</h1>
+                </div>
+                <div className='Education'>
+                    <div className='subhead'>
+                        <h1>
+                            Education
+                        </h1>
+                    </div>
+                    <div className='content'>
+                        <div className='image'>
+                            <img className='img' src={require('../../assets/Hamdard.jpeg')} alt='img' />
+                        </div>
+                        <div className='Contenttxt'>
+                            <p style={{ fontSize: 'X-Large', color: 'black' }} >An undergraduate student of Bachelors Science in Computer Science at Hamdard University Karachi. Undergratuating since 2021 Currently i am a 3rd year student 5th semester student</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
